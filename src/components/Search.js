a783a3e8097a70b81eb5dd0291c079af6746217d
naiiiden <na4ko359@gmail.com>
2022-05-13 21:55:23 +0300

    decorative arrow properly position inside input

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,8 +4,10 @@ const Search = () => {
     return (
         <form role="search" className="search--form">
             <label htmlFor="search" className="search--form--label">How can we help?</label>
-            <input type="search" placeholder="Search" id="search" className="search--form--input"/>
-            <Arrow className="arrow"/>
+            <div className="input--arrow--container">
+                <input type="search" placeholder="Search" id="search" className="search--form--input"/>
+                <Arrow className="arrow"/>
+            </div>
         </form>
     )
 }
