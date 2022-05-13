e56afd823c373c56783bf0ecfcd1c9204e419fe6
naiiiden <na4ko359@gmail.com>
2022-05-13 20:59:24 +0300

    header completed on desktop/buttons text put in <a> lead to real www.abstract.com webpage

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,8 +28,8 @@ const Header = () => {
                 </button>
             </div>
             <nav className={`nav--buttons ${toggleNavigation ? "" : "show"}`}>
-                <button className="header--nav--button--request">Submit a Request</button>
-                <button className="header--nav--button--sign-in">Sign in</button>
+                <button className="header--nav--button--request"><a href="https://help.abstract.com/hc/en-us/articles/360050382291-Contact-Support">Submit a Request</a></button>
+                <button className="header--nav--button--sign-in"><a href="https://help.abstract.com/hc/en-us/signin?return_to=https%3A%2F%2Fhelp.abstract.com%2Fhc%2Fen-us&locale=en-us">Sign in</a></button>
             </nav>
             <form role="search" className={`header--search--form ${toggleSearch ? "" : "show"}`}>
                 <input type="search" className="header--search--form--input" placeholder="Search"/>
