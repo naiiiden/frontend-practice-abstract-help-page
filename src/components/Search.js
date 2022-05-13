2c5b10a5bd3dbcc541419f3065c51e6c4d235697
naiiiden <na4ko359@gmail.com>
2022-05-13 21:46:19 +0300

    import arrow svg fill property works properly

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,11 @@
+import { ReactComponent as Arrow } from "../images/icon-arrow.svg";
+
 const Search = () => {
     return (
         <form role="search" className="search--form">
-            <label for="search" className="search--form--label">How can we help?</label>
+            <label htmlFor="search" className="search--form--label">How can we help?</label>
             <input type="search" placeholder="Search" id="search" className="search--form--input"/>
+            <Arrow className="arrow"/>
         </form>
     )
 }
