4ebc59ea7c738d5961af8440e1d9b16e40e927b2
naiiiden <na4ko359@gmail.com>
2022-05-17 16:47:31 +0300

    hero component add conditional render based on window width

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import Help_button from "../images/icon-help.svg";
 
 const Help = () => {
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1023);
+
+    const updateMedia = () => {
+        setIsDesktop(window.innerWidth > 1023)
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+    });
+
     return (
-        <button aria-label="Help" className="help--button" style={{background: "purple"}}>
-            <img src={Help_button} alt="" className="help--icon"/>
-            <span>Help</span>
-        </button>
+        <div>
+            {!isDesktop ? (
+                <button aria-label="Help" className="help--button" style={{background: "purple"}}>
+                <img src={Help_button} alt="" className="help--icon"/>
+            </button>
+            ) : (
+                <button aria-label="Help" className="help--button" style={{background: "purple"}}>
+                    <img src={Help_button} alt="" className="help--icon"/>
+                    <span>Help</span>
+                </button>
+            )}
+        </div>
     )
 }
 
