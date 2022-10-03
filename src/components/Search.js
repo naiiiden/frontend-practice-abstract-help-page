832b1531efc1eb5fd6e155b9207e4da63e131db4
naiiiden <na4ko359@gmail.com>
2022-10-03 06:00:24 +0300

    search.js delete unnecessary landmark role

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import { ReactComponent as Arrow } from "../images/icon-arrow.svg";
 
 const Search = () => {
     return (
-        <form role="search" className="search--form">
+        <form className="search--form">
             <label htmlFor="search" className="search--form--label">How can we help?</label>
             <div className="input--arrow--container">
                 <input type="search" placeholder="Search" id="search" className="search--form--input"/>
